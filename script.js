9fe1effa5875062dfa086c1f2ad92a75d05a90ea
Daris <aghnianmaela22@gmail.com>
2026-03-11 20:03:55 +0700

    feat: add function to display project info in console

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ function showMessage() {
     alert("Halo! Selamat datang di Praktikum Git Workflow Simulation.");
 }
 
+function showProjectInfo() {
+    alert("Project ini untuk latihan workflow Git di tim.");
+}
+
 console.log("Git Workflow Simulation loaded.");
