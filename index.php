762ae844b5bd93f516029f117798a2730ead25f5
iwangevelyna <iwangevelyna@MacBook-Pro-iwangevelyna.local>
2026-03-11 11:43:58 +0700

    fitur: menambahkan daftar kontributor tim

--- index.php
+++ index.php
@@ -12,10 +12,20 @@ $title = "Git Workflow Simulation";
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
+    
     <h1><?php echo $title; ?></h1>
 
     <p>Selamat datang di praktikum Git Workflow.</p>
+
+    <section>
+    <h2>Kontributor</h2>
+    <ul>
+        <li>Iwang Evelyna</li>
+        <li>Aghnian Maela Darissa'adah</li>
+        <li>Amalia Rizqi Kurniawati</li>
+    </ul>
+</section>
+
 
     <button onclick="showMessage()">Klik Saya</button>
 
